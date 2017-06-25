c3d7eb3321044f189a82587dd07c2164096fb069
Ekta Shahani <shahani.ekta@gmail.com>
2017-06-24 20:57:19 -0700

    Server side email functionality

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var email = require('../email.js');
+
+var router = express.Router();
+//'use strict';
+//const nodemailer = require('nodemailer');
+
+//router.use(bodyParser.raw({ type: 'text/xml' }));
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index.hbs', { title: 'Home' });
+});
+
+router.post('/submitemail', (req, res)=> {
+  console.log(req.body.email);
+  email.sendWelcomeMessage(req.body.email);
+  res.send('Email sent');
+});
+
+module.exports = router;
