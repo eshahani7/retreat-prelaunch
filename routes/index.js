60bf9abfab15f22ce78703c5d5b58ce67c2227d4
Ekta Shahani <shahani.ekta@gmail.com>
2017-06-25 16:46:37 -0700

    Render new front end

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,28 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const path = require('path');
 
 var email = require('../email.js');
 
 var router = express.Router();
+const publicPath = path.join(__dirname, '../public/index.html');
 //'use strict';
 //const nodemailer = require('nodemailer');
 
 //router.use(bodyParser.raw({ type: 'text/xml' }));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index.hbs', { title: 'Home' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('test.hbs', { title: 'Home' });
+// });
+
+router.use(express.static(publicPath));
 
 router.post('/submitemail', (req, res)=> {
   console.log(req.body.email);
-  email.sendWelcomeMessage(req.body.email);
-  email.subscribeEmail();
+  //email.sendWelcomeMessage(req.body.email);
+  email.subscribeEmail(req.body.email, req.body.fName, req.body.lName);
+  console.log(req.body.fName);
   res.send('Email sent');
 });
 
